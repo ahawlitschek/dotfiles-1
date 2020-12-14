013002f3bcde2bffadd0481d0f0066fa8767bd36
Alexander von Studnitz <studnitz@hey.com>
2020-12-14 11:01:10 +0100

    Updates

--- clone.sh
+++ clone.sh
@@ -14,3 +14,4 @@ git clone git@gitlab.com:clickbar/dgg-3d-platform.git $CLICKBAR/rapidcompact.com
 git clone git@gitlab.com:clickbar/clickbar-corporate-website.git $CLICKBAR/clickbar.dev
 git clone git@gitlab.com:clickbar/computerra/project-z.git $CLICKBAR/project-z
 git clone git@gitlab.com:clickbar/ead/portal-prototype.git $CLICKBAR/ead-portal
+git clone git@gitlab.com:clickbar/ffr/winterdient-planung.git $CLICKBAR/ffr-winterdienst
